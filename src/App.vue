229746e01bd2e3628f32f0335f93cab2008a5c2a
sister_clown <you@example.com>
2019-09-02 18:28:56 +0900

    change project style

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
-<template>
-
-  <div>
-    <button v-on:click="kanagawa('kanagawa')">kanagawaにすっぞ！！</button>
-    <label>
-      <input v-model="msg">
-    </label><br>
-      ==========================================================<br>
-      App1<br>
-      {{ msg }}<br>
-  </div>
+<template lang="pug">
+  div#app
+    div#nav
+      router-link(to="/")
+        span.linkText home
+      <router-link to="/">Homeeeee</router-link> |
+      <router-link to="/about">About</router-link>
+      <router-view/>
+      p kanagawa
 </template>
-<style scoped>
 
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    margin-right : 10px;
+  }
+  p {
+    color : red
+  }
+}
 </style>
-<script>
-    let msg = "message1";
-    export default {
-        data() {
-            return {
-                msg,
-            }
-        },
-        methods: {
-            kanagawa: function (test) {
-                this.msg = test
-            }
-        }
-    }
-</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+
+<template lang="pug">
+  div#home
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript Appppppppppppppp"/>
+    <HelloWorld2 msg="Welcome to Your Vue.js + TypeScript Appppppppppppppp"/>
+
+    <router-link to="/">Homeeeee</router-link> |
+    <router-link to="/about">About</router-link>
+        <router-view/>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld2.vue' // @ is an alias to /src
+import HelloWorld2 from '@/components/HelloWorld.vue'
+@Component({
+  components: {
+    HelloWorld,
+    HelloWorld2
+  }
+})
+export default class Home extends Vue {}
+</script>
